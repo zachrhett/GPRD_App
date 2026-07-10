ede4502e5f32f1725bfb99b8bf64ab8010b82fd8
Website-Public <nz78dj42c6@privaterelay.appleid.com>
2026-07-10 14:13:21 -0600

    Delete FRIEND_Working_SPA directory

--- FRIEND_Working_SPA/service-worker.js
+++
@@ -1,1 +0,0 @@
-const C='friend-demo-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./styles.css','./app.js']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
